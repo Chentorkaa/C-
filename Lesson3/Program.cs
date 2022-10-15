b2482c511dcc09c30f52721e3c924300325b2fa5
Chentorka <chentorka@gmail.com>
2022-10-15 11:52:57 +0300

    lesson3 Random

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -1,6 +1,6 @@
-﻿int numberA = new Random().Next(1, 10); //0123456789
-Console.WriteLine(numberA);
+﻿int numberC = new Random().Next(1, 10); //0123456789
+Console.WriteLine(numberC);
 int numberB = new Random().Next(1, 10);
 Console.WriteLine(numberB);
-int result = numberA+numberB;
+int result = numberC+numberB;
 Console.WriteLine(result);
